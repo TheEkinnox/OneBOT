0e4dadd91fe39e2887b4f35789a3e4fab0cc80b4
Loïck Noa OBIANG NDONG <theekinnox@gmail.com>
2019-12-09 23:55:51 -0500

    Added missing files

---
+++ OneBotNet/Core/Data/Classes/Item.cs
@@ -0,0 +1,44 @@
+﻿#region MÉTADONNÉES
+
+// Nom du fichier : Item.cs
+// Auteur :  (Loïck Obiang Ndong)
+// Date de création : 2019-09-20
+// Date de modification : 2019-09-20
+
+#endregion
+
+#region USING
+
+using System;
+using System.Threading.Tasks;
+
+#endregion
+
+namespace OneBotNet.Core.Data.Classes
+{
+    public class Item
+    {
+        #region PROPRIÉTÉS ET INDEXEURS
+
+        public string Nom { get; set; }
+        public string Description { get; set; }
+        public decimal PrixAchat { get; set; }
+        public decimal PrixVente { get; set; }
+
+        #endregion
+    }
+
+
+    public class ItemToSell : Item
+    {
+        #region PROPRIÉTÉS ET INDEXEURS
+
+        internal string NomVendeur { get; set; }
+        internal DateTime DateAjout { get; set; }
+        internal TimeSpan TempsAffichage { get; set; }
+        internal int Quantite { get; set; }
+
+
+        #endregion
+    }
+}
